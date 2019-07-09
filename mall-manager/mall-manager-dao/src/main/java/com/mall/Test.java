936a7938e3724525bab4f5be4353b33d78f34c34
zyftest1 <zyf1181373834@126.com>
2019-07-09 17:57:38 +0800

    项目初始化结构完成

---
+++ mall-manager/mall-manager-dao/src/main/java/com/mall/Test.java
@@ -0,0 +1,12 @@
+package com.mall;
+
+public class Test {
+    public static void main(String[] args) {
+        Student stu = new Student();
+
+    }
+
+    public void show(){
+
+    }
+}
---
+++ mall-manager/mall-manager-service/src/main/java/com/mall/ServiceTest.java
@@ -0,0 +1,8 @@
+package com.mall;
+
+public class ServiceTest {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.show();
+    }
+}

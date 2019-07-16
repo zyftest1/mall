7a430b435913c9a60576a62d792ea06ca87718c2
fjz <fjz@lanqiao.org>
2019-07-16 16:08:15 +0800

    fjz

--- mall-manager/mall-manager-dao/src/main/java/com/mall/service/ShopCarDao.java
+++ mall-manager/mall-manager-dao/src/main/java/com/mall/service/ShopCarDao.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface ShopCarDao {
-    List<BsShoppingCar> selectShoppingCar(String ID) throws SQLException;
+    public List<BsShoppingCar> selectShoppingCar(String ID) throws SQLException;
     void deleteShoppingCar(String carNumber);
     void updateShoppingCar(BsShoppingCar sCar) throws SQLException;
 }

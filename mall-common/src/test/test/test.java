37789bb29e3e4ad6b9dd40571d7e69b00e66fd60
JasonZha0 <43643536+JasonZha0@users.noreply.github.com>
2019-07-17 16:23:51 +0800

    1

---
+++ mall-common/src/test/test/test.java
@@ -0,0 +1,16 @@
+package test;
+
+import com.mall.common.utils.C3p0Utils2;
+
+import java.sql.SQLException;
+
+public class test {
+
+
+
+        public static void main(String[] args) {
+
+        }
+
+
+}
